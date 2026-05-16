c2d497ad7003d31ac3ee01822a51e874fbd2251d
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-16 11:21:09 -0700

    [CI] Fix phpunit issues.

--- tests/Tests/Classes/Provider/ServiceProviderClass.php
+++ tests/Tests/Classes/Provider/ServiceProviderClass.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Valkyrja PHPUnit package.
+ * This file is part of the Valkyrja Framework package.
  *
  * (c) Melech Mizrachi <melechmizrachi@gmail.com>
  *
@@ -25,15 +25,6 @@ final class ServiceProviderClass implements ServiceProviderContract
 
     public static bool $publishInterfaceCalled = false;
 
-    #[Override]
-    public static function publishers(): array
-    {
-        return [
-            ServiceProvidedClass::class     => [self::class, 'publish'],
-            ServiceProvidedInterface::class => [self::class, 'publishInterface'],
-        ];
-    }
-
     public static function publish(object $providerAware): void
     {
         self::$publishCalled = true;
@@ -42,5 +33,14 @@ final class ServiceProviderClass implements ServiceProviderContract
     public static function publishInterface(object $providerAware): void
     {
         self::$publishInterfaceCalled = true;
+    }
+
+    #[Override]
+    public function publishers(): array
+    {
+        return [
+            ServiceProvidedClass::class     => [self::class, 'publish'],
+            ServiceProvidedInterface::class => [self::class, 'publishInterface'],
+        ];
     }
 }
