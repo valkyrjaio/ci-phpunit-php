86e8b12c70170ab1bffafe0994aeb3140009c4ce
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-07-31 08:48:55 +0000

    [v26.5.0] chore: Update PhpUnitInfo for the release

--- src/PhpUnit/Constant/PhpUnitInfo.php
+++ src/PhpUnit/Constant/PhpUnitInfo.php
@@ -20,12 +20,12 @@ final class PhpUnitInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.4.2';
+    public const string VERSION = '26.5.0';
 
     /**
      * The PhpUnit package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'June 4 2026 09:46:15 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'July 31 2026 01:47:16 MST';
 }
